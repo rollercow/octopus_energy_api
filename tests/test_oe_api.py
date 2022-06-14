79b018151f6eb59cbcf4825433e67fab0a0887ec
Chris Jones <c.jones@swansea.ac.uk>
2022-06-14 21:53:31 +0100

    pre-commit-hooks, blackify

--- tests/test_oe_api.py
+++ tests/test_oe_api.py
@@ -4,26 +4,28 @@ from octopus_energy_api.api_interface import api
 
 from datetime import datetime, timedelta
 
+
 def test_init(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
 
-    expected = 'A-123AB123'
+    energy_api = oe_api("account_number", "api_key")
+
+    expected = "A-123AB123"
 
     actual = energy_api.account.number
 
     assert expected == actual
 
+
 def test_account_details(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
     energy_api.account_details()
 
-    expected = 'A-123AB123'
+    expected = "A-123AB123"
 
     actual = energy_api.account.number
 
@@ -32,31 +34,31 @@ def test_account_details(mocker):
 
 def test_datetime_conversion(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
     assert "2021-06-11T16:30:00" == energy_api.convert_datetime_to_tz(datetime(2021, 6, 11, 16, 30))
 
 
 def test_consumption(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_consumption)
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_consumption)
 
     today = datetime.today()
     start = today - timedelta(days=1)
 
     consumption = energy_api.consumption(start, today)
 
-    assert 0.405 == consumption[0]['consumption']
+    assert 0.405 == consumption[0]["consumption"]
 
     # test for failure when time difference too great
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_consumption)
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_consumption)
 
     today = datetime.today()
     start = today - timedelta(days=400)
@@ -67,12 +69,13 @@ def test_consumption(mocker):
     except RuntimeError:
         assert True
 
+
 def test_consumption_total(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_consumption)
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_consumption)
 
     today = datetime.today()
     start = today - timedelta(days=1)
@@ -81,12 +84,13 @@ def test_consumption_total(mocker):
 
     assert 2.71 == output
 
+
 def test_consumption_mean(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_consumption)
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_consumption)
 
     today = datetime.today()
     start = today - timedelta(days=1)
@@ -95,12 +99,13 @@ def test_consumption_mean(mocker):
 
     assert 0.18 == output
 
+
 def test_consumption_median(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_consumption)
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_consumption)
 
     today = datetime.today()
     start = today - timedelta(days=1)
@@ -109,12 +114,13 @@ def test_consumption_median(mocker):
 
     assert 0.082 == output
 
+
 def test_consumption_cost(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_consumption)
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_consumption)
 
     today = datetime.today()
     start = today - timedelta(days=1)
@@ -123,13 +129,14 @@ def test_consumption_cost(mocker):
 
     assert 7.3983 == output
 
+
 def test_meter_point(mocker):
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_account_details)
-    energy_api = oe_api('account_number', 'api_key')
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_account_details)
+    energy_api = oe_api("account_number", "api_key")
 
-    mocker.patch('octopus_energy_api.api_interface.api.run', return_value=sample_meter_data)
+    mocker.patch("octopus_energy_api.api_interface.api.run", return_value=sample_meter_data)
 
     expected = energy_api.account.mpan
 
-    assert expected == energy_api.meter_point()['mpan']
+    assert expected == energy_api.meter_point()["mpan"]
