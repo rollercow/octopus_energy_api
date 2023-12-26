e950dacb8f04a5a25f0895a0e6f096077aae6cc4
Chris Jones <c.jones@swansea.ac.uk>
2023-12-26 17:58:16 +0000

    always use UTC when talking to the API because thats easier than figuring out what format it wants for timezones

--- octopus_energy_api/meter.py
+++ octopus_energy_api/meter.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import pandas as pd
 
 
@@ -38,8 +38,8 @@ class meter:
 
     def consumption(self, start: datetime, end: datetime):
         """Get all consumption data between 2 datetimes"""
-        start = start.isoformat().replace("+00:00", "Z")
-        end = end.isoformat().replace("+00:00", "Z")
+        start = start.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
+        end = end.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
 
         url = self._api._urls().consumption_url(self.mpan, self.serial_number, start, end)
 
