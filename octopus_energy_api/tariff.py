f5aeaf7aa9ccbcce74c28e1bdf7b2650e691254d
Chris Jones <c.jones@swansea.ac.uk>
2023-12-30 20:40:53 +0000

    real tariff lookup function

--- octopus_energy_api/tariff.py
+++ octopus_energy_api/tariff.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timezone
+
+
 class tarrif:
     def __init__(self, api, tariff_code, fromDT, toDt):
         self._api = api
@@ -17,3 +20,15 @@ class tarrif:
             + ", GSP Group - "
             + self.GSPGroup
         )
+
+    def tariffCode(self):
+        return self.fuel + "-" + self.registers + "R-" + self.productCode + "-" + self.GSPGroup
+
+    def lookup(self, start: datetime, end: datetime):
+        if self.fuel == "E" and int(self.registers) == 1:
+            start = start.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
+            end = end.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
+            url = self._api._urls().tariff_url(self.productCode, self.tariffCode(), start, end)
+            return self._api._api.pageFetcher(url)
+        else:
+            print("Only single register electric meters implemented")
