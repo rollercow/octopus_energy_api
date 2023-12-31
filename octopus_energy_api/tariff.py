35c4423d0020b7d5686308a42af98935ac241d85
Chris Jones <c.jones@swansea.ac.uk>
2023-12-31 10:42:23 +0000

    Use sane default dates when looking up tarrif details

--- octopus_energy_api/tariff.py
+++ octopus_energy_api/tariff.py
@@ -2,12 +2,17 @@ from datetime import datetime, timezone
 
 
 class tarrif:
-    def __init__(self, api, tariff_code, fromDT, toDt):
+    def __init__(self, api, tariff_code, fromDT, toDT):
         self._api = api
         self.fuel = tariff_code[0]
         self.registers = tariff_code[2]
         self.productCode = tariff_code[5:-2]
         self.GSPGroup = tariff_code[-1]
+        self.fromDT = datetime.fromisoformat(fromDT)
+        if toDT:
+            self.toDT = datetime.fromisoformat(toDT)
+        else:
+            self.toDT = datetime.now(timezone.utc)
 
     def __str__(self):
         return (
@@ -24,7 +29,11 @@ class tarrif:
     def tariffCode(self):
         return self.fuel + "-" + self.registers + "R-" + self.productCode + "-" + self.GSPGroup
 
-    def lookup(self, start: datetime, end: datetime):
+    def lookup(self, start: datetime = None, end: datetime = None):
+        if not start:
+            start = self.fromDT
+        if not end:
+            end = self.toDT
         if self.fuel == "E" and int(self.registers) == 1:
             start = start.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
             end = end.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
