72c94966061754a29ddb4521c817aaa4f94b8b2b
Chris Jones <c.jones@swansea.ac.uk>
2024-01-01 22:55:06 +0000

    Trim the start end end dates of the tariff dataframe to match our join/leave dates

--- octopus_energy_api/tariff.py
+++ octopus_energy_api/tariff.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+import pandas as pd
 
 
 class tarrif:
@@ -38,6 +39,22 @@ class tarrif:
             start = start.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
             end = end.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
             url = self._api._urls().tariff_url(self.productCode, self.tariffCode(), start, end)
-            return self._api._api.pageFetcher(url)
+            dfPrice = self._api._api.pageFetcher(url)
+            dfPrice["valid_from"] = pd.to_datetime(dfPrice["valid_from"], utc=True)
+            dfPrice["valid_to"] = pd.to_datetime(dfPrice["valid_to"], utc=True)
+            # trim the end date to when we joined the tarrif
+            setend = {}
+            setend["valid_to"] = self.toDT
+            df2 = pd.DataFrame([setend])
+            dfPrice.update(df2)
+            # turn it upside down
+            dfPrice = dfPrice.sort_values("valid_from").reset_index(drop=True)
+            # trim the start date to when we joined the tarrif
+            setstart = {}
+            setstart["valid_from"] = self.fromDT
+            df2 = pd.DataFrame([setstart])
+            dfPrice.update(df2)
+
+            return dfPrice
         else:
             print("Only single register electric meters implemented")
